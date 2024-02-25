86138b24d1512152d7d62070e51203ada096b288
Felipe Navas <felipenavas.itec@gmail.com>
2024-02-24 22:32:01 -0300

    Add images for Work Experience

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,8 +1,10 @@
 import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
+import high_soft from './high_soft.png'
+import ibm from './ibm.svg'
+import itec from './itec.png'
+import kaver from './kaver.jpeg'
 import logo from './logo.svg'
+import naranjax from './naranjax.svg'
+import tarmac from './tarmac.ico'
 
-export { hero, meta, shopify, starbucks, tesla, logo }
+export { hero, high_soft, ibm, itec, kaver, logo, naranjax, tarmac }
